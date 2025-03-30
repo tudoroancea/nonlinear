49bebb8178c9e7324531d36c4750696d443da665
Ted <oancea.tudor@icloud.com>
2025-03-30 17:17:21 +0200

    fix bug for login
    Signed-off-by: Ted <oancea.tudor@icloud.com>

--- src/components/auth-provider.tsx
+++ src/components/auth-provider.tsx
@@ -1,39 +1,10 @@
 import { useTheme } from "@/hooks/use-theme";
-import { clerkUserIdAtom } from "@/lib/atoms/auth";
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ClerkProvider } from "@clerk/clerk-react";
 import { dark } from "@clerk/themes";
-import { useAtom } from "jotai";
-import { Suspense, useEffect } from "react";
-import { LoadingScreen } from "./loading-screen";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("PUBLISHABLE_KEY is not defined");
-}
-
-function AuthTokenHandler({ children }: { children: React.ReactNode }) {
-  const { userId, isSignedIn } = useAuth();
-  const [clerkUserId, setClerkUserId] = useAtom(clerkUserIdAtom);
-
-  useEffect(() => {
-    if (isSignedIn && userId) {
-      setClerkUserId(userId);
-    }
-  }, [isSignedIn, setClerkUserId, userId]);
-
-  // Only render the children once the clerkUserId has been set and we can start the chain
-  // fetch github token -> create graphql client -> query user data
-  return (
-    <>
-      {clerkUserId ? (
-        <Suspense fallback={<LoadingScreen message="Loading data..." />}>
-          {children}
-        </Suspense>
-      ) : (
-        <LoadingScreen message="Signing in..." />
-      )}
-    </>
-  );
 }
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
@@ -44,7 +15,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       afterSignOutUrl="/"
       appearance={{ baseTheme: theme == "light" ? undefined : dark }}
     >
-      <AuthTokenHandler>{children}</AuthTokenHandler>
+      {children}
     </ClerkProvider>
   );
 }
--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -1,27 +1,52 @@
+import { ErrorScreen } from "@/components/error-screen";
+import { AppSidebar } from "@/components/layout/app-sidebar";
+import { LoadingScreen } from "@/components/loading-screen";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { clerkUserIdAtom } from "@/lib/atoms/auth";
+import { useAuth } from "@clerk/clerk-react";
+import { useAtom } from "jotai";
+import { Suspense, useEffect } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { Outlet } from "react-router-dom";
-import { ErrorScreen } from "@/components/error-screen";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/layout/app-sidebar";
 
 export function AppLayout() {
+  const { userId } = useAuth();
+  const [clerkUserId, setClerkUserId] = useAtom(clerkUserIdAtom);
+
+  // Check that we are indeed signed in and set the clerk Id
+  useEffect(() => {
+    if (userId) {
+      setClerkUserId(userId);
+    }
+  }, [setClerkUserId, userId]);
+
+  // Only render the children once the clerkUserId has been set and we can start the chain
+  // fetch github token -> create graphql client -> query user data
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <div className="h-svh overflow-hidden lg:p-2 w-full">
-        <div className="lg:border lg:rounded-md overflow-hidden flex flex-col items-center justify-start bg-container h-full w-full">
-          <div className="w-full flex flex-col items-center">
-            <div className="w-full flex justify-between items-center border-b py-1.5 px-6 h-10">
-              <SidebarTrigger />
+    <>
+      {clerkUserId ? (
+        <Suspense fallback={<LoadingScreen message="Loading data..." />}>
+          <SidebarProvider>
+            <AppSidebar />
+            <div className="h-svh overflow-hidden lg:p-2 w-full">
+              <div className="lg:border lg:rounded-md overflow-hidden flex flex-col items-center justify-start bg-container h-full w-full">
+                <div className="w-full flex flex-col items-center">
+                  <div className="w-full flex justify-between items-center border-b py-1.5 px-6 h-10">
+                    <SidebarTrigger />
+                  </div>
+                </div>
+                <div className="overflow-auto h-[calc(100svh-80px)] lg:h-[calc(100svh-96px)] w-full">
+                  <ErrorBoundary fallback={<ErrorScreen />}>
+                    <Outlet />
+                  </ErrorBoundary>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="overflow-auto h-[calc(100svh-80px)] lg:h-[calc(100svh-96px)] w-full">
-            <ErrorBoundary fallback={<ErrorScreen />}>
-              <Outlet />
-            </ErrorBoundary>
-          </div>
-        </div>
-      </div>
-    </SidebarProvider>
+          </SidebarProvider>
+        </Suspense>
+      ) : (
+        <LoadingScreen message="Signing in..." />
+      )}
+    </>
   );
 }
